67b1942a2cc6d543a070ee4a6654b0872c2f033d
Lefteris Aslanoglou <leftos@gmail.com>
2011-09-27 18:54:23 +0300

    With the help of Teo Chatzigiannakis, we've rebuilt the sound controls into a new dedicated AmbientSoundControl, which has all the code needed for each audio file. The random button has been rewritten as well to use the MaybePlay() method of each AmbientSoundControl.

---
+++ Relax/AmbientSoundControl.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using IrrKlang;
+
+namespace Relax
+{
+    /// <summary>
+    /// Interaction logic for AmbientSoundControl.xaml
+    /// </summary>
+    public partial class AmbientSoundControl : UserControl
+    {
+        public ISoundEngine Engine { get; set; }
+        public ISound Sound { get; protected set; }
+
+        private string _internalName;
+        public string InternalName
+        {
+            get
+            {
+                return this._internalName;
+            }
+            set
+            {
+                this._internalName = value;
+            }
+        }
+        public string FriendlyName
+        {
+            get
+            {
+                return this.EnableThisSound.Content.ToString();
+            }
+            set
+            {
+                this.EnableThisSound.Content = value;
+            }
+        }
+        public string FileName
+        {
+            get
+            {
+                return "res/{f}.ogg".Replace("{f}", InternalName);
+            }
+        }
+        public float Volume
+        {
+            get
+            {
+                if (this.Sound != null) return this.Sound.Volume;
+                else return 0;
+            }
+            set
+            {
+                if (this.Sound != null) this.Sound.Volume = value;
+            }
+        }
+
+        public AmbientSoundControl()
+        {
+            InitializeComponent();
+        }
+        public AmbientSoundControl(ISoundEngine engine, string internalName, string friendlyName) : this()
+        {
+            this.Engine = engine;
+            this.InternalName = internalName;
+            this.FriendlyName = friendlyName;
+        }
+
+        private void Play()
+        {
+            //try
+            //{
+                this.Sound = this.Engine.Play2D(this.FileName, true, true);
+                this.Sound.Paused = false;
+            //}
+            //catch
+            //{
+            //}
+        }
+        private void Stop()
+        {
+            this.Sound.Stop();
+        }
+
+        private void EnableThisSound_Checked(object sender, RoutedEventArgs e)
+        {
+            this.Play();
+        }
+        private void EnableThisSound_Unchecked(object sender, RoutedEventArgs e)
+        {
+            this.Stop();
+        }
+
+        private void VolumeOfThisSound_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            this.Volume = (float)this.VolumeOfThisSound.Value / 100.0f;
+        }
+
+        public bool MaybePlay()
+        {
+            this.EnableThisSound.IsChecked = new Random().Next(2) == 0 && this.KeepThisSound.IsChecked.Value;
+            return (bool)this.EnableThisSound.IsChecked;
+        }
+    }
+}
--- Relax/MainWindow.xaml.cs
+++ Relax/MainWindow.xaml.cs
@@ -22,169 +22,78 @@ namespace Relax
     public partial class MainWindow : Window
     {
         protected ISoundEngine engine = new ISoundEngine();
-        protected ISound sndAlarm;
-        protected ISound sndAC;
-        protected ISound sndAquarium;
-        protected ISound sndAtmospheric;
-        protected ISound sndBasstone;
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void chkAlarm_Checked(object sender, RoutedEventArgs e)
-        {            
-            sndAlarm = engine.Play2D("res/Alarm.ogg", true, true);
-            sndAlarm.Volume = (float)sldAlarm.Value / 100.0f;
-            sndAlarm.Paused = false;
+        List<AmbientSoundControl> controls = new List<AmbientSoundControl>();
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (UIElement c in this.mainGrid.Children)
+            { 
+                if (c is AmbientSoundControl)
+                {
+                    controls.Add((AmbientSoundControl) c);
+                }
+            }
+
+            this.SetEngine();
         }
 
-        private void chkAlarm_Unchecked(object sender, RoutedEventArgs e)
+        private void SetEngine()
         {
-            sndAlarm.Stop();
+            foreach (AmbientSoundControl c in controls)
+                c.Engine = this.engine;
         }
 
-        private void sldAlarm_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        public static void Shuffle(IList list)
         {
-            if (sndAlarm != null)
+            Random rng = new Random();
+            int n = list.Count;
+            while (n > 1)
             {
-                sndAlarm.Volume = (float)sldAlarm.Value / 100.0f;
+                n--;
+                int k = rng.Next(n + 1);
+                object value = list[k];
+                list[k] = list[n];
+                list[n] = value;
             }
         }
 
-        private void chkAC_Checked(object sender, RoutedEventArgs e)
+        private void Randomize(int count)
         {
-            sndAC = engine.Play2D("res/Airconditioner.ogg", true, true);
-            sndAC.Volume = (float)sldAC.Value / 100.0f;
-            sndAC.Paused = false;
-        }
-
-        private void chkAC_Unchecked(object sender, RoutedEventArgs e)
-        {
-            sndAC.Stop();
-        }
-
-        private void sldAC_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            if (sndAC != null)
+            Shuffle(controls);
+            StopAllSounds();
+            int picked = 0;
+            foreach (AmbientSoundControl c in controls)
             {
-                sndAC.Volume = (float)sldAC.Value / 100.0f;
+                if (c.MaybePlay()) picked++;
+                if (picked == count) break;
             }
+            if (picked == 0)
+                Randomize(count);
         }
 
-        private void chkAquarium_Checked(object sender, RoutedEventArgs e)
+        private void StopAllSounds()
         {
-            sndAquarium = engine.Play2D("res/Aquarium.ogg", true, true);
-            sndAquarium.Volume = (float)sldAquarium.Value / 100.0f;
-            sndAquarium.Paused = false;
-        }
-
-        private void chkAquarium_Unchecked(object sender, RoutedEventArgs e)
-        {
-            sndAquarium.Stop();
-        }
-
-        private void sldAquarium_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            if (sndAquarium != null)
+            foreach (AmbientSoundControl c in controls)
             {
-                sndAquarium.Volume = (float)sldAquarium.Value / 100.0f;
+                c.EnableThisSound.IsChecked = false;
             }
         }
 
         private void sldMasterVolume_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            engine.SoundVolume = (float)sldMasterVolume.Value / 100.0f;
-        }
-
-        private void chkAtmospheric_Checked(object sender, RoutedEventArgs e)
-        {
-            sndAtmospheric = engine.Play2D("res/Atmospheric.ogg", true, true);
-            sndAtmospheric.Volume = (float)sldAtmospheric.Value / 100.0f;
-            sndAtmospheric.Paused = false;
-        }
-
-        private void chkAtmospheric_Unchecked(object sender, RoutedEventArgs e)
-        {
-            sndAtmospheric.Stop();
-        }
-
-        private void sldAtmospheric_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            if (sndAtmospheric != null)
-            {
-                sndAtmospheric.Volume = (float)sldAtmospheric.Value / 100.0f;
-            }
-        }
-
-        private void chkBasstone_Checked(object sender, RoutedEventArgs e)
-        {
-            sndBasstone = engine.Play2D("res/Basstone.ogg", true, true);
-            sndBasstone.Volume = (float)sldBasstone.Value / 100.0f;
-            sndBasstone.Paused = false;
-        }
-
-        private void chkBasstone_Unchecked(object sender, RoutedEventArgs e)
-        {
-            sndBasstone.Stop();
-        }
-
-        private void sldBasstone_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            if (sndBasstone != null)
-            {
-                sndBasstone.Volume = (float)sldBasstone.Value / 100.0f;
-            }
+            this.engine.SoundVolume = (float)sldMasterVolume.Value / 100.0f;
         }
 
         private void btnRandom_Click(object sender, RoutedEventArgs e)
         {
-            ArrayList keeps = new ArrayList();
-
-            if (keepAC.IsChecked == true) { keeps.Add("AC"); }
-            if (keepAlarm.IsChecked == true) { keeps.Add("Alarm"); }
-            if (keepAquarium.IsChecked == true) { keeps.Add("Aquarium");  }
-            if (keepAtmospheric.IsChecked == true) { keeps.Add("Atmospheric");  }
-            if (keepBasstone.IsChecked == true) { keeps.Add("Basstone");  }
-
-            // Choose a random number of items from the ones kept for random choice
-            if (keeps.Count == 0) return;
-
-            chkAC.IsChecked = false;
-            chkAlarm.IsChecked = false;
-            chkAquarium.IsChecked = false;
-            chkAtmospheric.IsChecked = false;
-            chkBasstone.IsChecked = false;
-
-            Random randomGen = new Random();
-            int count = randomGen.Next(keeps.Count) + 1;
-
-            ArrayList choices = new ArrayList(count);
-            int choice;
-
-            for (int i = 0; i < count; i++)
-            {
-                choice = randomGen.Next(keeps.Count);
-                switch ((string)keeps[choice])
-                {
-                    case "AC":
-                        chkAC.IsChecked = true;
-                        break;
-                    case "Alarm":
-                        chkAlarm.IsChecked = true;
-                        break;
-                    case "Aquarium":
-                        chkAquarium.IsChecked = true;
-                        break;
-                    case "Atmospheric":
-                        chkAtmospheric.IsChecked=true;
-                        break;
-                    case "Basstone":
-                        chkBasstone.IsChecked = true;
-                        break;
-                }
-            }
+            if (iudRandom.Value.HasValue)
+                Randomize((int)iudRandom.Value);
         }
+
     }
 }
